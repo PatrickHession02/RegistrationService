ef8638f8f5a7aeb4532400a31b4f901dd028207f
PATRICK HESSION <G00394619@gmit.ie>
2023-10-16 23:29:23 +0100

    Fixing registration Controller

--- src/main/java/ie/atu/registrationservice/RegistrationController.java
+++ src/main/java/ie/atu/registrationservice/RegistrationController.java
@@ -1,10 +1,13 @@
 package ie.atu.registrationservice;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegistrationController {
-    public void RegRequests(){
+    @PostMapping("/confirm")
+    public String confirmDetails(@RequestBody UserDetails)
 
-    }
 }
